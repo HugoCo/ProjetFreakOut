dfeb608493bb1291c1dc7dac653362df8a01d5e4
el98 <elisefu@hotmail.com>
2019-12-18 14:21:16 +0100

    Update freakOut.py

--- freakOut.py
+++ freakOut.py
@@ -1,46 +1,54 @@
 import random
+from queue import Queue
+
 
 class Card:
 
     def __init__(self, num, color):
 
-        self.num=num
-        self.color=color
+        self.num = num
+        self.color = color
 
     def __str__(self):
         return "La carte est:" + str(self.num) + " " + self.color
 
+
 class Player(Process):
-    def __init__(self):
+    def __init__(self, Pile):
+        for i in range(5):
+
 
 class Board:
-    def __init__(self, card,numPlayers):
-        self.card=card
-        processes=[]
-        for in range (numPlayers):
-            p=Player()
+    def __init__(self, card, numPlayers):
+        self.card = card
+        processes = []
+        for in range(0, numPlayers):
+            p = Player()
             processes.append(p)
 
-        
 
-    
+"""
 class Pile:
     def __init__(self):
-        self.cardList= []
+        self.cardList = []
         for i in range(2):
-            for j in range(1,11):
-                if i==0:
+            for j in range(1, 11):
+                if i == 0:
                     self.cardList.append(Card(j, "blue"))
                 else:
-                    self.cardList.append(Card(j,"red"))
+                    self.cardList.append(Card(j, "red"))
 
         random.shuffle(self.cardList)
-    
+"""
 
 if __name__ == "__main__":
-    l=Pile()
-    for i in range(20):
-        print(l.cardList[i])
 
-
-
+    # Initialisation Pile
+    cardPile = []
+    for i in range(2):
+        for j in range(1, 11):
+            if i == 0:
+                cardPile.append(Card(j, "blue"))
+            else:
+                cardPile.append(Card(j, "red"))
+    random.shuffle(cardPile)
