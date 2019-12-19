4b0bc2184afa0bf1596891eea00c912895f001d7
Hugo <hug1910@live.fr>
2019-12-19 15:49:42 +0100

    MESSAGE

--- freakOut.py
+++ freakOut.py
@@ -1,33 +1,32 @@
 import random
+from multiprocessing import Process, Value, Array
 from queue import Queue
 
 
-class Card:
+"""class Card:
 
-    def __init__(self, num, color):
+    def __init__(self, numCol):
 
-        self.num = num
-        self.color = color
+        self.numCol=numCol
 
     def __str__(self):
         return "La carte est:" + str(self.num) + " " + self.color
+"""
 
-
-class Player(Process):
-    def __init__(self, Pile):
-        for i in range(5):
+#class Player(Process, ID):
+ #   def __init__(self, Pile):
 
 
 class Board:
     def __init__(self, card, numPlayers):
         self.card = card
         processes = []
-        for in range(0, numPlayers):
+        for i in range(0, numPlayers):
             p = Player()
             processes.append(p)
 
 
-"""
+"""     
 class Pile:
     def __init__(self):
         self.cardList = []
@@ -44,11 +43,17 @@ class Pile:
 if __name__ == "__main__":
 
     # Initialisation Pile
-    cardPile = []
-    for i in range(2):
+    pile=Array('i',range(20))
+    for i in range (-10,11): #les numéros négatifs représenteront les bleus tandis que les numéros négatifs seront les rouges    
+        if(i!=0):
+            pile[i]=i
+    
+    """for i in range(2):
         for j in range(1, 11):
             if i == 0:
                 cardPile.append(Card(j, "blue"))
             else:
                 cardPile.append(Card(j, "red"))
-    random.shuffle(cardPile)
+    """
+    random.shuffle(pile)
+    print(pile)
