73891412da7b0ed0fce446e148a3bf2263e43928
el98 <elisefu@hotmail.com>
2019-12-19 16:58:58 +0100

    yes

---
+++ TestMethod.py
@@ -0,0 +1,9 @@
+def is_valid(board_card, player_card):
+    if ((player_card == board_card + 1)
+        or (player_card == board_card - 1)
+            or (abs(player_card) == abs(board_card))):
+        return True
+    return False
+
+    if __name__ == "__main__":
+        print(is_valid(10, 11))
--- freakOut.py
+++ freakOut.py
@@ -1,6 +1,5 @@
 import random
 from multiprocessing import Process, Value, Array
-from queue import Queue
 
 
 """class Card:
@@ -13,8 +12,8 @@ from queue import Queue
         return "La carte est:" + str(self.num) + " " + self.color
 """
 
-#class Player(Process, ID):
- #   def __init__(self, Pile):
+# class Player(Process, ID):
+#   def __init__(self, Pile):
 
 
 class Board:
@@ -26,34 +25,21 @@ class Board:
             processes.append(p)
 
 
-"""     
-class Pile:
-    def __init__(self):
-        self.cardList = []
-        for i in range(2):
-            for j in range(1, 11):
-                if i == 0:
-                    self.cardList.append(Card(j, "blue"))
-                else:
-                    self.cardList.append(Card(j, "red"))
+def is_valid(board_card, player_card):
+    if ((player_card == board_card + 1)
+        or (player_card == board_card - 1)
+            or (abs(player_card) == abs(board_card))):
+        return True
+    return False
 
-        random.shuffle(self.cardList)
-"""
 
 if __name__ == "__main__":
 
     # Initialisation Pile
-    pile=Array('i',range(20))
-    for i in range (-10,11): #les numéros négatifs représenteront les bleus tandis que les numéros négatifs seront les rouges    
-        if(i!=0):
-            pile[i]=i
-    
-    """for i in range(2):
-        for j in range(1, 11):
-            if i == 0:
-                cardPile.append(Card(j, "blue"))
-            else:
-                cardPile.append(Card(j, "red"))
-    """
+    pile = Array('i', range(-11, 11))
+    print(pile[:])
+    for i in range(-10, 11):  # les numéros négatifs représenteront les bleus tandis que les numéros négatifs seront les rouges
+        if(i != 0):
+            pile[i] = i
     random.shuffle(pile)
     print(pile)
