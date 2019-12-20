123e9741bee165655f82810580be359fca0e07be
Hugo <hug1910@live.fr>
2019-12-20 14:41:27 +0100

    t

--- freakOut.py
+++ freakOut.py
@@ -15,15 +15,45 @@ mq_PtoB = sysv_ipc.MessageQueue(key_PtoB)
 
 
 class Board:
-    def __init__(self, numCard, numPlayers,lock):
+    def __init__(self, numCard, numPlayers, lock):
         self.card = numCard
         processes = []
         for i in range(0, numPlayers):
             p = Player()
             processes.append(p)
-            #il faut start les processes
+            # il faut start les processes
+
     def run(self):
+        message=0
         while(! is__finished()):
+            #Message queue Board to Player
+            while message:
+                message_BtoP = str(value_BtoP).encode()
+                mq_BtoP.send(message_BtoP)
+                
+
+            # Message Queue Player to Board
+            while True:
+                message_PtoB, t = mq_PtoB.receive()
+                value_PtoB = message_PtoB.decode()
+                value_PtoB = int(value_PtoB)
+                if value_PtoB: #Value_PtoB sera un tableau avec 2 cases : la première est la valeur de la carte, la 2e, le numéro du joueur
+                    print("received:", value_PtoB)
+                    numJoueur=value_PtoB[1]
+                    if is_valid(self.card,value_PtoB[0]):                        
+                        self.card=value_PtoB[0]
+                        message=1
+                        value_BtoP=int(value_PtoB[0])
+                    else:
+                        value_BtoP=int(100+value_PtoB[0]) # Si mauvais on renvoie le numéro de la carte + 100
+                    mq_PtoB.empty()
+
+                else:
+                    print("exiting.")
+                    break
+        mq_BtoP.remove()
+        mq_PtoB.remove()
+            
             
 
 def is_finished(pile,lock):
@@ -38,7 +68,7 @@ def is_valid(board_card, player_card):
         return True
     return False
 
-def pioche(pile):
+def pioche(pile,lock):
     pile_content=pile[0]
     pile.pop(0)
     return pile_content
@@ -49,27 +79,8 @@ def pioche(pile):
 
 
 if __name__ == "__main__":
-    #Creating Message Queue Board to Player
-    value_BtoP = 1
-    while value_BtoP:
-        try:
-            value_BtoP = int(input())
-        except:
-            print("Input error, try again!")
-        message_BtoP = str(value_BtoP).encode()
-        mq_BtoP.send(message_BtoP)
-    mq_BtoP.remove()
+    # Creating Message Queue Board to Player
 
-    #Creating Message Queue Player to Board
-    while True:
-        message_PtoB, t = mq_PtoB.receive()
-        value_PtoB = message_PtoB.decode()
-        value_PtoB = int(value_PtoB)
-        if value_PtoB:
-            print("received:", value_PtoB)
-        else:
-            print("exiting.")
-            break
 
     # Initialisation Pile
     # les numéros négatifs représenteront les bleus tandis que les numéros négatifs seront les rouges
