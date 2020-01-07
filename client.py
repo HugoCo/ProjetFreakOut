83ec549edf33ce7da494c8da48e4ca65c9ae2876
el98 <elisefu@hotmail.com>
2020-01-07 14:10:13 +0100

    OKOK

--- client.py
+++ client.py
@@ -7,18 +7,7 @@ mq_BtoP = sysv_ipc.MessageQueue(key_BtoP)
 mq_PtoB = sysv_ipc.MessageQueue(key_PtoB, sysv_ipc.IPC_CREAT)
 
 if __name__ == "__main__":
-    #Creating Message Queue Board to Player (client)
-    while True:
-        message_BtoP, t = mq_BtoP.receive()
-        value_BtoP = message_BtoP.decode()
-        value_BtoP = int(value_BtoP)
-        if value_BtoP:
-            print("received:", value_BtoP)
-        else:
-            print("exiting.")
-            break
-
-    #Creating Message Queue Player to Board (server)
+    # Creating Message Queue Player to Board (server)
     value_PtoB = 1
     while value_PtoB:
         try:
@@ -29,3 +18,14 @@ if __name__ == "__main__":
         mq_PtoB.send(message_PtoB)
 
     mq_PtoB.remove()
+
+    # Creating Message Queue Board to Player (client)
+    while True:
+        message_BtoP, t = mq_BtoP.receive()
+        value_BtoP = message_BtoP.decode()
+        value_BtoP = int(value_BtoP)
+        if value_BtoP:
+            print("received:", value_BtoP)
+        else:
+            print("exiting.")
+            break
